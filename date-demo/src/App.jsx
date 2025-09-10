2580cc8c70a2871dc094d53d10792e85f1e485b3
Alexander <alexanderm.kodehode@gmail.com>
2025-09-10 11:03:28 +0200

    Managed to push a minimal working page

--- date-demo/src/App.jsx
+++ date-demo/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { format, formatDistance, formatRelative, subDays } from "date-fns";
 function App() {
-  const [count, setCount] = useState(0)
-
+  const myName = "Alexander";
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>{myName}</h1>
+      <p>{format(new Date(), "dd MMMM yyyy")}</p>
+      <p>{formatDistance(subDays(new Date(), 3), new Date())} ago</p>
+      <p>{formatRelative(subDays(new Date(), 3), new Date())} ago</p>
+      <p>Open the console to see date outputs</p>
+    </div>
+  );
 }
-
-export default App
+export default App;
