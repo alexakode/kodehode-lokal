0ade3a9eed87c426af6923b213d953c904a54618
Alexander <alexanderm.kodehode@gmail.com>
2025-09-10 11:22:08 +0200

    Translated dates into Norwegian using date-fns/locale

--- date-demo/src/App.jsx
+++ date-demo/src/App.jsx
@@ -3,15 +3,26 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { format, formatDistance, formatRelative, subDays } from "date-fns";
+import { nb } from "date-fns/locale";
 function App() {
   const myName = "Alexander";
   return (
     <div>
-      <h1>{myName}</h1>
-      <p>{format(new Date(), "dd MMMM yyyy")}</p>
-      <p>{formatDistance(subDays(new Date(), 3), new Date())} ago</p>
-      <p>{formatRelative(subDays(new Date(), 3), new Date())} ago</p>
-      <p>Open the console to see date outputs</p>
+      <h1>{myName} er en const i denne filen, App.jsx</h1>
+      <p>{format(new Date(), "dd MMMM yyyy", { locale: nb })} kommer fra format i date-fns</p>
+      <p>
+        {formatDistance(subDays(new Date(), 3), new Date(), { locale: nb })} siden kommer fra
+        formatDistance og subDays i date-fns
+      </p>
+      <p>
+        {formatRelative(subDays(new Date(), 3), new Date(), { locale: nb })} siden kommer fra
+        formatRelative og subDays i date-fns.
+      </p>
+      <p>
+        Strengene i App.jsx er skrevet på norsk bokmål fordi jeg har importert locale nb fra
+        date-fns/locale.
+      </p>
+      <p>Disse eksporteres som App</p>
     </div>
   );
 }
